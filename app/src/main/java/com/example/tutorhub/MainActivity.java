5ac676e32163d409474ad82cb5514fa32b6957b0
Tejas Vissapragada <tejvissa@ravinders-air.lan>
2020-06-27 16:14:58 -0700

    Initial commit

---
+++ app/src/main/java/com/example/tutorhub/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tutorhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
